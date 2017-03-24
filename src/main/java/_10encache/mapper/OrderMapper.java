3ea78c2c25d685b4a434688c2a80c1e7e7544796
Aceysx <aceysx@163.com>
2017-03-24 10:15:55 +0800

    update lazyLoading

---
+++ src/main/java/_10encache/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package _10encache.mapper;
+
+import _10encache.po.Order;
+
+import java.util.List;
+
+/**
+ * Created by acey on 17-3-23.
+ */
+public interface OrderMapper {
+     List<Order> findOrders();
+}
